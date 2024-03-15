e11f2ff7fb95ea54661f4a1e997bb5839666326b
HajimeFuji <fujiihyrk@gmail.com>
2024-03-15 11:00:44 +0900

    コンタクトフォーム作成

--- contact.php
+++ contact.php
@@ -5,12 +5,12 @@ Template Post Type: page, post
 */
 ?>
 
-<?php get_header();?>
+<?php get_header('noheader');?>
 
 <section class=top-wrap>
     <h1>事業者の方へ</h1>
     <p>情報の追加、修正などについては<br>お気軽にお問い合わせください。</p>
-        <?php echo do_shortcode('[contact-form-7 id="f50bce3" title="Contact form 1"]'); ?>
+        <?php echo do_shortcode('[wpforms id="918"]'); ?>
 </section>
 
 <?php get_footer();?>
